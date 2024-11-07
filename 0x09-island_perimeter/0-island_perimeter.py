d2253ec2c7f61b876fb26aa5dbd24122f3ee7f7f
Blossom Israel Ayogu <ayogublossomisrael@gmail.com>
2024-11-07 03:21:00 +0100

    Task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+"""contains isliand_perimeter function
+"""
+
+
+def island_perimeter(grid):
+    """A function to calculate the perimeter of the island described in grid.
+
+    Args:
+        grid (list): a list of lists of integers where 0 represents water and 1 represent land
+
+    Returns:
+        (int): the perimeter of the island described in grid
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
